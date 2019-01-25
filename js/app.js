aa9f099f12dd82f14eb703119cf7eda6b7c1dcdf
Levi <levi_blodgett@outlook.com>
2019-01-24 21:08:47 -0600

    Finished project for now, might add error catching sometime

--- js/app.js
+++ js/app.js
@@ -1,13 +1,36 @@
+// Init storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData();
 // Init weather object
-const weather = new Weather('Sioux Falls', 'SD');
+const weather = new Weather(weatherLocation.city);
+// Init UI
+const ui = new UI();
 
 // Get weather on DOM load
-document.addEventListener('DOMContentLoaded', getWeather);
+document.addEventListener('DOMContentLoaded',getWeather);
 
-// weather.changeLocation('Miami', 'FL');
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+
+  // Change location
+  weather.changeLocation(city);
+
+  // Set location in local storage
+  storage.setLocationData(city);
+
+  // Get and display weather
+  getWeather();
+
+  // Close modal
+  $('#locModal').modal('hide');
+});
 
 function getWeather() {
   weather.getWeather()
-  .then(results => console.log(results))
+  .then(results => {
+    ui.paint(results);
+  })
   .catch(err => console.log(err));
 }
--- js/weather.js
+++ js/weather.js
@@ -1,15 +1,14 @@
 class Weather {
-  constructor(city, state) {
+  constructor(city) {
     this.apiKey = 'acfaa97e3a5ebca90f088482cdda4664';
     this.city = city;
-    this.state = state;
   }
 
-  // Fetch weather from API
+  // Fetch weather from API 
   async getWeather() {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}&units=imperial`);
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}&units=imperial`)
 
-    const responseData = await response.json;
+    const responseData = await response.json();
 
     return responseData;
   }
